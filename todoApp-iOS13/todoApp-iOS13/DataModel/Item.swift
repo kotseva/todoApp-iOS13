a32a168b38525100996cf7a511145e250a4e5231
Angela Koceva <angelakoceva@outlook.com>
2020-02-14 21:57:27 +0100

    Implementing UserDefaults

---
+++ todoApp-iOS13/todoApp-iOS13/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  todoApp-iOS13
+//
+//  Created by Angela Koceva on 2/14/20.
+//  Copyright © 2020 Angela Koceva. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
